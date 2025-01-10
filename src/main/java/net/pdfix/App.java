1a9147c01e708af8934c45b000f476b29a6dbaca
jbaranec <jozef.baranec@gmail.com>
2025-01-10 21:49:57 +0100

    added option '-d' to process whole directory, output formatting

--- src/main/java/net/pdfix/App.java
+++ src/main/java/net/pdfix/App.java
@@ -1,52 +1,150 @@
 package net.pdfix;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Properties;
 
 public class App {
-        private static void displayVersion() {
-                Properties properties = new Properties();
-                try {
-                        properties.load(App.class.getClassLoader().getResourceAsStream("version.properties"));
-                        String version = properties.getProperty("project.version");
-                        String name = properties.getProperty("project.name");
-                        System.out.println(name + " v" + version);
-                } catch (IOException e) {
-                        System.err.println("Error reading version information.");
-                }
+  private static void displayVersion() {
+    Properties properties = new Properties();
+    try {
+      properties.load(App.class.getClassLoader().getResourceAsStream("version.properties"));
+      String version = properties.getProperty("project.version");
+      String name = properties.getProperty("project.name");
+      System.out.println(name + " v" + version + "\n");
+    } catch (IOException e) {
+      System.err.println("Error reading version information.");
+    }
+  }
+
+  public static boolean isPDFFile(String filePath) {
+    File file = new File(filePath);
+
+    if (!file.exists() || !file.isFile()) {
+      System.out.println("File does not exist or is not a regular file: " + filePath);
+      return false;
+    }
+
+    try (FileInputStream fis = new FileInputStream(file)) {
+      byte[] buffer = new byte[5];
+      int bytesRead = fis.read(buffer);
+
+      if (bytesRead < 5) {
+        System.out.println("File is too small to be a valid PDF: " + filePath);
+        return false;
+      }
+
+      String fileSignature = new String(buffer, "UTF-8");
+      return "%PDF-".equals(fileSignature);
+    } catch (IOException e) {
+      System.out.println("Error reading file: " + e.getMessage());
+      return false;
+    }
+  }
+
+  // Collects all files from the specified directory
+  private static List<File> collectFiles(String directoryPath) {
+    List<File> fileList = new ArrayList<>();
+    try {
+      Files.walk(Paths.get(directoryPath))
+          .filter(Files::isRegularFile) // Include only regular files (not directories)
+          .forEach(path -> fileList.add(path.toFile()));
+    } catch (IOException e) {
+      System.err.println("Error reading files: " + e.getMessage());
+    }
+    return fileList;
+  }
+
+  private static void processFile(File file) throws Exception {
+    // Process single file
+    System.out.println("File: " + file.getPath() + "");
+
+    if (!isPDFFile(file.getAbsolutePath())) {
+      System.out.println("Not a PDF file");
+      return;
+    }
+
+    int count = FindDuplicateMcid.checkDuplicateMcid(file.getAbsolutePath(), false);
+    if (count == 0) {
+      System.out.println("No duplicate MCIDs found");
+    } else {
+      System.out.println(String.format("Total %d duplicate MCIDs found", count));
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    displayVersion();
+
+    String inputFile = "";
+    String inputDirectory = "";
+    String last = "";
+
+    for (String s : args) {
+      if (!last.isEmpty()) {
+        // expected parameter name in last
+        if (last.equals("-i")) {
+          inputFile = s;
+        } else if (last.equals("-d")) {
+          inputDirectory = s;
         }
-
-        public static void main(String[] args) throws Exception {
-                displayVersion();
-
-                String inputFile = "";
-                String last = "";
-                for (String s : args) {
-                        if (!last.isEmpty()) {
-                                // expected parameter name in last
-                                if (last == "-i") {
-                                        inputFile = s;
-                                }
-                        } else {
-                                if (s == "--help") {
-                                        System.out.println("Show help ...");
-                                        return;
-                                        // show version
-                                } else if ((s.compareTo("-i") == 0) || (s.compareTo("--input") == 0)) {
-                                        last = "-i";
-                                }
-                        }
-                }
-
-                try {
-                        if (inputFile.isEmpty()) {
-                                throw new RuntimeException("Missing input file. See --help for cli parameters");
-                        }
-                        if (FindDuplicateMcid.checkDuplicateMcid(inputFile, false) == 0) {
-                                System.out.println("No duplicate MCIDs found");
-                        }
-                } catch (Exception e) {
-                        System.out.println(e.getLocalizedMessage());
-                }
+        last = ""; // reset last after parameter is set
+      } else {
+        if (s.equals("--help")) {
+          System.out.println("Usage:");
+          System.out.println("  -i <file>    : Process a single PDF file.");
+          System.out.println("  -d <folder>  : Process all PDF files in a folder.");
+          return;
+        } else if (s.equals("-i") || s.equals("--input")) {
+          last = "-i";
+        } else if (s.equals("-d") || s.equals("--directory")) {
+          last = "-d";
         }
+      }
+    }
+
+    try {
+      List<File> fileList = new ArrayList<>();
+      if (!inputFile.isEmpty()) {
+        fileList.add(new File(inputFile));
+      } else if (!inputDirectory.isEmpty()) {
+        fileList = collectFiles(inputDirectory);
+      }
+
+      // Sort the files
+      Collections.sort(fileList, new Comparator<File>() {
+        @Override
+        public int compare(File f1, File f2) {
+          // Compare file paths first
+          int pathComparison = f1.getAbsolutePath().compareTo(f2.getAbsolutePath());
+
+          // If paths are the same, compare file names
+          if (pathComparison == 0) {
+            return f1.getName().compareTo(f2.getName());
+          }
+          return pathComparison;
+        }
+      });
+
+      // Process each file
+      for (File file : fileList) {
+        System.out.println("===============================================================================");
+        try {
+          processFile(file);
+        } catch (Exception e) {
+          System.out.println(e.getLocalizedMessage());
+        }
+        System.out.println("===============================================================================\n");
+      }
+      System.out.println("Process complete");
+    } catch (Exception e) {
+      System.out.println(e.getLocalizedMessage());
+    }
+  }
 }
--- src/main/java/net/pdfix/FindDuplicateMcid.java
+++ src/main/java/net/pdfix/FindDuplicateMcid.java
@@ -6,99 +6,115 @@ import java.util.List;
 import net.pdfix.pdfixlib.*;
 
 public class FindDuplicateMcid {
-    // Helper function to get a readable object type
-    private static String getNiceObjType(PdfPageObjectType type) {
-        switch (type) {
-            case kPdsPageText:
-                return "text";
-            case kPdsPagePath:
-                return "path";
-            case kPdsPageImage:
-                return "image";
-            case kPdsPageShading:
-                return "shading";
-            case kPdsPageForm:
-                return "form";
-            default:
-                return "unknown";
-        }
+  // Helper function to get a readable object type
+  private static String getNiceObjType(PdfPageObjectType type) {
+    switch (type) {
+      case kPdsPageText:
+        return "text";
+      case kPdsPagePath:
+        return "path";
+      case kPdsPageImage:
+        return "image";
+      case kPdsPageShading:
+        return "shading";
+      case kPdsPageForm:
+        return "form";
+      default:
+        return "unknown";
+    }
+  }
+
+  // Helper function to get object information
+  private static String getObjBBox(PdsPageObject obj) {
+    StringBuilder info = new StringBuilder();
+    PdfRect bbox = obj.GetBBox();
+    info.append(String.format("[%.2f, %.2f, %.2f, %.2f]", bbox.left, bbox.bottom, bbox.right, bbox.top));
+    return info.toString();
+  }
+
+  private static String getObjContent(PdsPageObject obj) {
+    StringBuilder info = new StringBuilder();
+    if (obj.GetObjectType() == PdfPageObjectType.kPdsPageText) {
+      PdsText textObj = (PdsText) obj;
+      info.append(textObj.GetText());
+    }
+    return info.toString();
+  }
+
+  // Check for duplicate MCIDs in a PDF file. Return the number of dulicate mcids
+  // found
+  public static int checkDuplicateMcid(String path, boolean autotag) throws Exception {
+    Pdfix pdfix = new Pdfix();
+
+    PdfDoc doc = pdfix.OpenDoc(path, "");
+    if (doc == null) {
+      throw new RuntimeException(pdfix.GetError());
     }
 
-    // Helper function to get object information
-    private static String getObjInfo(PdsPageObject obj) {
-        StringBuilder info = new StringBuilder();
-        PdfRect bbox = obj.GetBBox();
-        info.append(String.format("BBox: [%.2f, %.2f, %.2f, %.2f]\n", bbox.left, bbox.bottom, bbox.right, bbox.top));
+    int found = 0;
 
-        if (obj.GetObjectType() == PdfPageObjectType.kPdsPageText) {
-            PdsText textObj = (PdsText) obj;
-            info.append("Text: ").append(textObj.GetText()).append("\n");
-        }
-
-        return info.toString();
+    if (autotag) {
+      PdfTagsParams params = new PdfTagsParams();
+      doc.RemoveTags();
+      doc.RemoveStructTree();
+      doc.AddTags(params);
     }
 
-    // Check for duplicate MCIDs in a PDF file. Return the number of dulicate mcids found
-    public static int checkDuplicateMcid(String path, boolean autotag) throws Exception {
-        Pdfix pdfix = new Pdfix();
-        System.out.println(
-                "PDFix SDK " + pdfix.GetVersionMajor() + "." + pdfix.GetVersionMinor() + "." + pdfix.GetVersionPatch());
+    for (int i = 0; i < doc.GetNumPages(); i++) {
+      PdfPage page = doc.AcquirePage(i);
+      if (page == null) {
+        System.out.println("Warning: Unable to load page " + (i + 1));
+        continue;
+      }
 
-        PdfDoc doc = pdfix.OpenDoc(path, "");
-        if (doc == null) {
-            throw new RuntimeException(pdfix.GetError());
-        }
+      PdsContent content = page.GetContent();
+      if (content == null) {
+        page.Release();
+        continue;
+      }
 
-        int found = 0;
+      int lastMcid = -1;
+      List<Integer> mcids = new ArrayList<Integer>();
+      for (int j = 0; j < content.GetNumObjects(); j++) {
+        PdsPageObject obj = content.GetObject(j);
+        int mcid = obj.GetMcid();
+        if (mcid != lastMcid) {
+          lastMcid = mcid;
+          if (mcid == -1) {
+            continue;
+          }
 
-        if (autotag) {
-            PdfTagsParams params = new PdfTagsParams();
-            doc.RemoveTags();
-            doc.RemoveStructTree();
-            doc.AddTags(params);
-        }
+          if (mcids.contains(mcid)) {
+            System.out.println("Duplicate MCID Found:");
+            String objType = getNiceObjType(obj.GetObjectType());
+            String objBBox = getObjBBox(obj);
+            String objContent = getObjContent(obj);
 
-        for (int i = 0; i < doc.GetNumPages(); i++) {
-            PdfPage page = doc.AcquirePage(i);
-            if (page == null) {
-                System.out.println("Warning: page " + (i + 1) + " is null in " + path);
-                continue;
+            StringBuilder info = new StringBuilder();
+            info.append(String.format("  %-10s: %d\n", "MCID", mcid));
+            info.append(String.format("  %-10s: %d\n", "Page", i + 1));
+            info.append(String.format("  %-10s: %d\n", "Index", j));
+            info.append(String.format("  %-10s: %s\n", "Type", objType));
+            info.append(String.format("  %-10s: %s\n", "BBox", objBBox));
+            if (!objContent.isEmpty()) {
+              String truncatedContent = objContent.length() > 80 ? objContent.substring(0, 80) + "…"
+                  : objContent;
+              info.append(String.format("  %-10s: %s\n", "Content", truncatedContent));
             }
 
-            PdsContent content = page.GetContent();
-            if (content == null) {
-                page.Release();
-                continue;
-            }
-
-            int lastMcid = -1;
-            List<Integer> mcids = new ArrayList<Integer>();
-            for (int j = 0; j < content.GetNumObjects(); j++) {
-                PdsPageObject obj = content.GetObject(j);
-                int mcid = obj.GetMcid();
-                if (mcid != lastMcid) {
-                    lastMcid = mcid;
-                    if (mcid == -1) {
-                        continue;
-                    }
-
-                    if (mcids.contains(mcid)) {
-                        String objType = getNiceObjType(obj.GetObjectType());
-                        String objInfo = getObjInfo(obj);
-                        System.out.printf("Duplicate MCID: '%d'\nPage: %d, Index: %d, Type: %s\n%s\n", mcid, i + 1, j,
-                                objType, objInfo);
-                        found ++;
-                    } 
-                    mcids.add(mcid);
-                }
-            }
-
-            page.Release();
+            System.out.println(info.toString());
+            found++;
+          }
+          mcids.add(mcid);
         }
+      }
 
-        doc.Close();
-        pdfix.Destroy();
-
-        return found;
+      page.Release();
     }
+
+    doc.Close();
+    pdfix.Destroy();
+
+    return found;
+  }
 }
