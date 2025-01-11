dd4fac9b30ac28eae0ec565ca4088db828ae4bc6
jbaranec <jozef.baranec@gmail.com>
2025-01-11 17:09:59 +0100

    added mvn test - FindDuplicateMcidTest.java

---
+++ src/test/java/net/pdfix/FindDuplicateMcidTest.java
@@ -0,0 +1,20 @@
+package net.pdfix;
+
+import org.junit.jupiter.api.Test;
+
+public class FindDuplicateMcidTest {
+
+  // Test validation of dulicate MCID 
+  // test.pdf contains a duplicate MCID '0' with content 'Multi-Platform PDF Library SDK'
+  // on top of the page
+
+  @Test
+  void testDuplicateMcid() throws Exception {
+    String basePath = System.getProperty("user.dir"); // path to current folder
+    String pdfPath = basePath + "/resources/test.pdf";
+    int ret = FindDuplicateMcid.checkDuplicateMcid(pdfPath, false);
+    if (ret != 1) {
+      throw new Exception("testDuplicateMcid Failed");
+    }
+  }
+}
