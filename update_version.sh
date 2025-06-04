35d451bcf7dc596a5ec3e64bf159f8992a9cc1be
Peter Kral <peter.kral@pdfix.net>
2025-06-04 10:22:54 +0200

    config.json - added name update_version.sh - added comments - added echo and fix echo versions

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script is used to update the version number in the config.json file
+# during the maven build process in GitHub Actions.
+# It replace "0.0.0" in program arguments with current version of container.
+# It also replaces the version number in the config.json file.
+
 # Check if an argument is provided
 if [ -z "$1" ]; then
     echo "No argument provided. Usage: ./update_version.sh v1.2.3"
@@ -27,7 +32,11 @@ echo "Short version: ${version_short}"
 mvn versions:set -DnewVersion=${version_short}
 
 # Replace "v0.0.0" placeholder with the provided argument in config.json
-sed -i "s|v0.0.0|$1|g" config.json
-sed -i "s|0.0.0|$version_short|g" config.json
+sed -i "s|v0\.0\.0|$1|g" config.json
 
-echo "Replaced all occurrences of 'latest' with '$1' in config.json."
+echo "Replaced all occurrences of 'v0.0.0' with '$1' in config.json."
+
+# Replace "0.0.0" placeholder with the provided argument in config.json
+sed -i "s|0\.0\.0|$version_short|g" config.json
+
+echo "Replaced all occurrences of '0.0.0' with '$1' in config.json."
